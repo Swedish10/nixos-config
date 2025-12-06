e58ce75a552633a9d3506a766a8ab44957f2a267
Swedish10 <knordstrom115@gmail.com>
2025-12-06 20:26:49 +0000

    added configurations for other 2 macminis

---
+++ hosts/firstfleet/skiff2/default.nix
@@ -0,0 +1,36 @@
+{ config, lib, pkgs, ... }:
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware.nix
+      ../../../modules/core/default.server.nix
+    ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "swix-skiff2"; # Define your hostname.
+  # Pick only one of the below networking options.
+  networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  # networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+
+  # Set your time zone.
+  time.timeZone = "America/NewYork";
+
+  #enable flakes
+  nix.settings.experimental-features = [ "nix-command" "flakes"];
+
+  #
+  #   Do NOT change this value unless you have manually inspected 
+  # all the changes it would make to your configuration, and
+  # migrated your data accordingly.
+  # For more information, see:
+  #  `man configuration.nix`
+  #            or
+  #   https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
+  #
+
+  system.stateVersion = "25.05"; # Did you read the comment?
+}
+
