24bf0b3ad947dfb01360f34155080447a9a92f92
Swedish10 <knordstrom115@gmail.com>
2025-12-14 03:12:15 +0000

    configured nodes

--- hosts/firstfleet/fleetnode.nix
+++ hosts/firstfleet/fleetnode.nix
@@ -6,4 +6,11 @@
     defaultGateway = "192.168.1.1";
     nameservers = [ "1.1.1.1" "8.8.8.8" ];
   };
+
+  services.k3s = {
+    enable = true;
+    role = "agent";
+    serverAddr = "https://192.168.1.10:6443";
+    token = "K100a42eae3b99f8a4ae40a2ec4ad137c96d984d601429fc0bc220bcbe4c5789652::server::502c692de38dcc84fdcd666404eea7ef";
+  };
 }
